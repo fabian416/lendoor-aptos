b647b6bdfda83f034b69f32533e72e48ba30fecb
fabiandiaz416 <diazfabian99@icloud.com>
2025-08-30 13:41:08 -0300

    update RiskManagerUncollat

--- contracts/src/Vault/RiskManagerUncollat.sol
+++ contracts/src/Vault/RiskManagerUncollat.sol
@@ -16,7 +16,7 @@ abstract contract RiskManagerUncollatModule is IRiskManager, LiquidityUtils {
     error E_InvalidAddress();
 
     // Default CLM address 
-    address internal constant DEFAULT_CLM = 0xe4e642e106364b55b9406106E8CB5D8A9fF4e500;
+    address internal constant DEFAULT_CLM = 0x2164C00409dE4F9b73EF6fa7365470Dd60f7F4bF;
 
     // Storage in the VAULT (written via delegatecall)
     address private _creditLimitManager; // 0 = uses DEFAULT_CLM
